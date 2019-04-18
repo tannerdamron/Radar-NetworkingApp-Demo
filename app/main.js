5bcee10cdea970517a58d4d8d1be3bcf36bb100b
Young Liu <youngliu92@gmail.com>
2019-04-18 02:57:02 -0700

    add login page and styling.

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 import store from './store';
+import LoginPage from './components/LoginPage'
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
@@ -8,5 +9,5 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h('frame', [h(LoginPage)])
 }).$start()
