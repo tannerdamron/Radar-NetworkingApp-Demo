64f494fbc5ecee3c50a11eb7b3152fe7ff2898ae
Young Liu <youngliu92@gmail.com>
2019-04-18 05:31:26 -0700

    add ability to log out.

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -4,13 +4,30 @@
     </ActionBar>
     <StackLayout dock="top" height="90%" width="100%" style="">
       <Label text="Proficient in: JS, C#, Vue, Angular, NativeScript" />
+      <Button  text="Logout" class="logout" @tap="logoutApp()" />
     </StackLayout>
   </Page>
 </template>
 
 <script>
+import LoginPage from "./LoginPage.vue";
+import {mapState} from "vuex";
 export default {
-  
+  computed: {
+    ...mapState(["isLoggedIn"])    
+  },
+  methods: {
+    logoutApp() {
+      this.$authService.logout()
+    }
+  },
+  watch: {
+    isLoggedIn(val) {
+      if (!val) {
+        this.$navigateTo(LoginPage, { clearHistory: true });
+      }
+    }
+  }
 }
 </script>
 
